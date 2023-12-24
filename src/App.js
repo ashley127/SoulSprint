cb0d72678fbc2f66e7d3f8454c73b3edd05ec65c
Ashley Zhao <zhaoashley2005@gmail.com>
2023-12-24 12:44:12 -0500

    Create todo form

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ToDoForm from './components/ToDoForm';
+
+function App() {
+  return (
+    <div className="App">
+    <ToDoForm/>
+    </div>
+  );
+}
+
+export default App;
